c2dc753951f649a14db624de1b6adcb0ee91bbc5
Unknown <yobojo@moruzza.com>
2019-02-15 16:49:34 +0100

    Add some logic

--- mnk.py
+++ mnk.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+import copy
 X = 1
 O = 2
 k = 4
@@ -10,10 +11,6 @@ grid = [[0 for j in range(initial_grid_size)] for i in range(initial_grid_size)]
 
 grid[center[0]][center[1]] = X
 grid[center[0]][center[1]+1] = O
-grid[center[0]+1][center[1]] = X
-grid[center[0]+2][center[1]] = O
-grid[center[0]-1][center[1]] = X
-grid[center[0]-2][center[1]] = O
 #Location for first 2 moves do not matter due to rotational symmetry
 #O and X should always move next to each other since not doing so will always make
 #X's grid value smaller, since O will not obstruct anything
@@ -21,7 +18,7 @@ grid[center[0]-2][center[1]] = O
 directions = [[0,4,6],
 			  [1,5,7],
 			  [2,4,5],
-			  [3,5,6],
+			  [3,6,7],
 			  [0,2,4,5,6],
 			  [1,2,4,5,7],
 			  [0,3,4,6,7],
@@ -31,11 +28,15 @@ directions = [[0,4,6],
 def gameOver():
 	pprint(grid)
 	exit()
-
-# left, right, up, down, ul, ur, dl, dr, no = 0, 1, 2, 3, 4, 5, 6, 7, 8
 def getNeighbours(dir):
 	return directions[dir]
 	
+def getValue(pos):
+	return grid[pos[0]][pos[1]]
+	
+def setValue(pos, value):
+	grid[pos[0]][pos[1]] = value
+
 # 0     1      2   3     4   5   6   7
 # left, right, up, down, ul, ur, dl, dr	
 def getPosFromDir(pos, dir):
@@ -56,8 +57,8 @@ def getPosFromDir(pos, dir):
 	elif dir == 7:
 		return (pos[0] + 1, pos[1] - 1)
 				
-# left, right, up, down, ul, ur, dl, dr, no = 0, 1, 2, 3, 4, 5, 6, 7, 8			
 def getAllOptions(pos, dir):
+	# TODO : Fix cycles
 	result = set([])
 	for direction in getNeighbours(dir):
 		cell = getPosFromDir(pos, direction)
@@ -74,29 +75,54 @@ def getAllOptions(pos, dir):
 # the grid value of player X
 # Proof: uit me duim gezogen
 	
-def bestMove(player):
-    if (player == X):
-		for x in getAllOptions((initial_grid_size/2, initial_grid_size/2), 8):
-			grid[x[0]][x[1]] = 9
+def doBestMove(player):
+	if (player == X):
+		max = 0
+		bestPos = (0,0)
+		for x in getAllOptions(center, 8):
+			tmpgrid = copy.deepcopy(grid)
+			tmpgrid[x[0]][x[1]] = X
+			gridworth = getGridValue(tmpgrid, X)
+			
+			if gridworth == -1:
+				setValue(x, X)
+				pprint(grid)
+				exit()
+				
+			if gridworth > max:
+				bestPos = x
+				max = gridworth
+		setValue(bestPos, X)
 		pprint(grid)
-		return
-        #for each possible option
+		#for each possible option
 		#	maximize getGridValue for that option for player X
 		#	if multiple options have the same getGridValue
 		#		select option with minimal getGridValue for player O
 		#do that move
 		#check whether game is over
-    else:  
-		return
-        #for each possible option
+	else:  
+		min = 9999999999999999999
+		bestPos = (0,0)
+		for x in getAllOptions(center, 8):
+			tmpgrid = copy.deepcopy(grid)
+			tmpgrid[x[0]][x[1]] = O
+			gridworth = getGridValue(tmpgrid, X)
+			if gridworth < min:
+				bestPos = x
+				min = gridworth
+		setValue(bestPos, O)
+		pprint(grid)
+		#for each possible option
 		#	minimize getGridValue for that option for player X
 		#	if multiple options have the same getGridValue
 		#		select option with maximal getGridValue for player O
 		#do that move
 
 # How to calculate grid value: idk
-def getGridValue(grid, player):
-	# TODO: figure out how gridvalue should be calculated
+def getGridValue(newgrid, player):
+	# TODO: alles
 	return
 	
-bestMove(X)
+while True:
+	doBestMove(X)
+	doBestMove(O)
